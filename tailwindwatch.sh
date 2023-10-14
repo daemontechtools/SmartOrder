da1f1fea96ab8d173971ed1ec4c4351522add2a7
Josh Elias <josh@daemontech.tools>
2023-10-14 09:33:29 -0400

    run tailwind in github action

--- tailwindwatch.sh
+++ tailwindwatch.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-tailwindPath="/usr/bin/tailwindcss"
-appStylesPathIn="./Styles/main.css"
+if [ "$ASPNETCORE_ENVIRONMENT" == "Production" ]; then
+  appStylesPathIn="./Styles/main.production.css"
+else
+  appStylesPathIn="./Styles/main.development.css"
+fi
+
 appStylesPathOut="./wwwroot/css/bundle.css"
 
 
 if ! pgrep tailwindcss > /dev/null; then
-  nohup $tailwindPath -i $appStylesPathIn -o $appStylesPathOut --watch > /dev/null 2>&1 &
+  nohup $TAILWINDCSS -i $appStylesPathIn -o $appStylesPathOut --watch > /dev/null 2>&1 &
 fi
