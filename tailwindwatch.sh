f09fb0452b171111904cb63311e027657bc2abd0
Josh Elias <josh@daemontech.tools>
2023-10-20 16:23:08 -0400

    use env var for TAILWINDCSS everywhere

--- tailwindwatch.sh
+++ tailwindwatch.sh
@@ -5,16 +5,14 @@ appStylesPathIn="./Styles/styles.css"
 appStylesPathOut="./wwwroot/css/styles.css"
 
 if [ "$APP_MODE" == "Production" ]; then
-
   echo "Building for production"
   $TAILWINDCSS -i $appStylesPathIn -o $appStylesPathOut
 
 else
   echo "Building for development"
-  
   if ! pgrep tailwindcss > /dev/null; then
     echo "Starting tailwindcss"
-    /usr/bin/tailwindcss -i $appStylesPathIn -o $appStylesPathOut
-    /usr/bin/tailwindcss -i $appStylesPathIn -o $appStylesPathOut --watch &
+    $TAILWINDCSS -i $appStylesPathIn -o $appStylesPathOut
+    $TAILWINDCSS -i $appStylesPathIn -o $appStylesPathOut --watch &
   fi
 fi
