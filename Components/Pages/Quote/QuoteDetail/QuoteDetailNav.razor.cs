023de6aff107380ae8c208f023ad42360ff25140
Josh Elias <josh@daemontech.tools>
2023-11-13 20:43:51 -0500

    update QuoteDetail nav and header

---
+++ Components/Pages/Quote/QuoteDetail/QuoteDetailNav.razor.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Components;
+
+namespace SmartEstimate.Pages;
+
+
+public struct QuoteDetailNavProps {
+    public int QuoteId { get; set; }
+    public int QuoteDocumentCount { get; set; }
+} 
+
+public partial class QuoteDetailNav : ComponentBase {
+
+    [Parameter] 
+    public QuoteDetailNavProps? Props { get; set; }
+
+    private (string, string)[] MenuOptions { get; set; } = new (string, string)[]{};
+
+    protected override void OnInitialized() {
+        MenuOptions = new (string, string)[] {
+            ("Rooms", $"/quotes/{Props?.QuoteId}"),
+            ("Shipping", $"/quotes/{Props?.QuoteId}/shipping"),
+            ($"Documents ({Props?.QuoteDocumentCount})", $"/quotes/{Props?.QuoteId}/documents"),
+        };
+    }
+}
