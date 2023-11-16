8d95274b328ab43bfd55a4edc8fc545e27a2df6c
Josh Elias <josh@daemontech.tools>
2023-11-15 19:57:46 -0500

    add ProductType and ProductAddOn

--- Components/Models/Product/Product.cs
+++ Components/Models/Product/Product.cs
@@ -3,6 +3,23 @@
 
 namespace SmartEstimate.Models;
 
+
+public enum ProductType
+{
+    Upper,
+    Base,
+    Tall,
+    Vanity,
+    Panel,
+    Moulding,
+    Accessory
+}
+
+public struct ProductAddOn {
+    public string Name { get; set; }
+    public List<string> Options { get; set; }
+}
+
 public struct Product : IDbModel
 {
     public int Id { get; set; }
