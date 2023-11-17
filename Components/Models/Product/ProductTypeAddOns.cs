21a135741388d1ed26165ccd37c3fe4ddff9e133
Josh Elias <josh@daemontech.tools>
2023-11-16 21:55:36 -0500

    extend ProductDetail page

---
+++ Components/Models/Product/ProductTypeAddOns.cs
@@ -0,0 +1,56 @@
+namespace SmartEstimate.Models;
+
+public struct ProductTypeAddOns {
+    public Dictionary<ProductType, List<ProductAddOn>> TypeAddOns { get; set; }
+
+    public ProductTypeAddOns() {
+        TypeAddOns = new Dictionary<ProductType, List<ProductAddOn>>();
+
+        TypeAddOns.Add(ProductType.Upper, new List<ProductAddOn>
+        {
+            new ProductAddOn { Name = "Hinging", Options = new List<string> { "Option1", "Option2" } },
+            new ProductAddOn { Name = "Left End", Options = new List<string> { "Option1", "Option2" } },
+            new ProductAddOn { Name = "Right End", Options = new List<string> { "Option1", "Option2" } }
+        });
+
+        TypeAddOns.Add(ProductType.Base, new List<ProductAddOn>
+        {
+            new ProductAddOn { Name = "Hinging", Options = new List<string> { "Option1", "Option2" } },
+            new ProductAddOn { Name = "Left End", Options = new List<string> { "Option1", "Option2" } },
+            new ProductAddOn { Name = "Right End", Options = new List<string> { "Option1", "Option2" } },
+            new ProductAddOn { Name = "Interior Drw Top", Options = new List<string> { "Option1", "Option2" } },
+            new ProductAddOn { Name = "Interior Drw Btm", Options = new List<string> { "Option1", "Option2" } }
+        });
+
+        TypeAddOns.Add(ProductType.Tall, new List<ProductAddOn>
+        {
+            new ProductAddOn { Name = "Hinging", Options = new List<string> { "Option1", "Option2" } },
+            new ProductAddOn { Name = "Left End", Options = new List<string> { "Option1", "Option2" } },
+            new ProductAddOn { Name = "Right End", Options = new List<string> { "Option1", "Option2" } },
+            new ProductAddOn { Name = "Interior Drw Top", Options = new List<string> { "Option1", "Option2" } },
+            new ProductAddOn { Name = "Interior Drw Btm", Options = new List<string> { "Option1", "Option2" } }
+        });
+
+        TypeAddOns.Add(ProductType.Vanity, new List<ProductAddOn>
+        {
+            new ProductAddOn { Name = "Hinging", Options = new List<string> { "Option1", "Option2" } },
+            new ProductAddOn { Name = "Left End", Options = new List<string> { "Option1", "Option2" } },
+            new ProductAddOn { Name = "Right End", Options = new List<string> { "Option1", "Option2" } }
+        });
+
+        TypeAddOns.Add(ProductType.Panel, new List<ProductAddOn>
+        {
+
+        });
+
+        TypeAddOns.Add(ProductType.Moulding, new List<ProductAddOn>
+        {
+
+        });
+
+        TypeAddOns.Add(ProductType.Accessory, new List<ProductAddOn>
+        {
+
+        });
+    }
+}
