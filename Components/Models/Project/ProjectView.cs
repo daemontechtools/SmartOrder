f58f16563f4e905185ec184d2d519650ce7d1890
Josh Elias <josh@daemontech.tools>
2023-12-20 09:19:59 -0500

    Add RequiredIfAttribute and update form validation

--- Components/Models/Project/ProjectView.cs
+++ Components/Models/Project/ProjectView.cs
@@ -17,8 +17,41 @@ public class ProjectView : SMARTBaseClass {
     public bool IsShipped { get; set; } = false;
     public string LinkIDEmployeeSoldBy { get; set; }
 
-  
+    
+    [RequiredIf(
+        "IsShipped", 
+        true, 
+        ErrorMessage = "Shipping Location is required when IsShipped is true."
+    )]
+    //[Required]
     public ShipLocationView ProjectShipLocation { get; set; }
 
     public IList<ProjectGroupView> ProjectGroups { get; set; }
+}
+
+
+public class RequiredIfAttribute : ValidationAttribute
+{
+    private readonly string _propertyName;
+    private readonly object _expectedValue;
+
+    public RequiredIfAttribute(string propertyName, object expectedValue)
+    {
+        _propertyName = propertyName;
+        _expectedValue = expectedValue;
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var instance = validationContext.ObjectInstance;
+        var type = instance.GetType();
+        var propertyValue = type?.GetProperty(_propertyName)?.GetValue(instance, null);
+
+        if (propertyValue?.ToString() == _expectedValue.ToString() && value == null)
+        {
+            return new ValidationResult(ErrorMessage, new[] { validationContext.MemberName });
+        }
+
+        return ValidationResult.Success;
+    }
 }
