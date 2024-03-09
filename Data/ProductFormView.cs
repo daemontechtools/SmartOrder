12475213a3d2fa16a7b5c795e4f0ea808fb0c070
Josh Elias <josh@daemontech.tools>
2024-03-09 11:05:22 -0500

    remove warnings in Form Views set Form View proterties to default!

--- Data/ProductFormView.cs
+++ Data/ProductFormView.cs
@@ -1,26 +1,24 @@
 
 using SMART.Common.ProductionManagement;
-
 namespace SO.Data;
 
 public class ProductFormView : SMARTBaseClassView {
-    public ProductFormView(string LinkID) : base(LinkID) { }
-    
-    public ProductTypes Type { get; set; }
-    public string CategoryName { get; set; }
-    public string LinkIDCategory { get; set; }
-    public string Name { get; set; }
-    public string Comments { get; set; }
-    public float PriceLibrary { get; set; }
-    public string ProductDoorSwing { get; set; }
-    public string ProductDoorStyle { get; set; }
-    public string ProductLeftSide { get; set; }
-    public string ProductRightSide { get; set; }
-    public string ProductFinishExterior { get; set; }
-    public string ProductFinishInterior { get; set; }
-    public string ProductSlide { get; set; }
-    public float Width { get; set; }
-    public float Height { get; set; }
-    public float Depth { get; set; }
-    public float Quantity { get; set; }
+    public ProductFormView(string LinkID) : base(LinkID) {}
+    public ProductTypes Type { get; set; } = default!;
+    public string CategoryName { get; set; } = default!;
+    public string LinkIDCategory { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public string Comments { get; set; } = default!;
+    public float PriceLibrary { get; set; } = default!;
+    public string ProductDoorSwing { get; set; } = default!;
+    public string ProductDoorStyle { get; set; } = default!;
+    public string ProductLeftSide { get; set; } = default!;
+    public string ProductRightSide { get; set; } = default!;
+    public string ProductFinishExterior { get; set; } = default!;
+    public string ProductFinishInterior { get; set; } = default!;
+    public string ProductSlide { get; set; } = default!;
+    public float Width { get; set; } = default!;
+    public float Height { get; set; } = default!;
+    public float Depth { get; set; } = default!;
+    public float Quantity { get; set; } = default!;
 }
