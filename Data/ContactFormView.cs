3c5f1568c3d98758be222bbf222d8d392f119294
Josh Elias <josh@daemontech.tools>
2024-03-14 10:58:22 -0400

    fix(ui): render ShipLocationForm

--- Data/ContactFormView.cs
+++ Data/ContactFormView.cs
@@ -3,13 +3,10 @@ using SMART.Common.CompanyManagement;
 namespace SO.Data;
 
 public class ContactFormView : SMARTBaseClassView {
-    public ContactFormView(string LinkID) : base(LinkID) { 
-        DefaultEmail = new CommunicationLink("");
-        DefaultPhone = new CommunicationLink("");
-    }
+    public ContactFormView(string LinkID) : base(LinkID) {}
 
     [Required]
     public string DisplayName { get; set; } = default!;
-    public CommunicationLink DefaultEmail { get; set; } = default!;
-    public CommunicationLink DefaultPhone { get; set; } = default!;
+    public CommunicationLink DefaultEmail { get; set; } = new();
+    public CommunicationLink DefaultPhone { get; set; } = new();
 }
