76f3077140e7eae66a82d08901eb29d427b8fff0
Josh Elias <josh@daemontech.tools>
2024-03-21 12:09:55 -0400

    refactor(form): isolate RequiredIfAttribute

--- Data/ProjectFormView.cs
+++ Data/ProjectFormView.cs
@@ -26,33 +26,3 @@ public class ProjectFormView : SMARTBaseClass {
 
     public IList<ProjectGroup> ProjectGroups { get; set; } = new List<ProjectGroup>();
 }
-
-public class RequiredIfAttribute : ValidationAttribute {
-    private readonly string _propertyName;
-    private readonly object _expectedValue;
-
-    public RequiredIfAttribute(string propertyName, object expectedValue) {
-        _propertyName = propertyName;
-        _expectedValue = expectedValue;
-    }
-
-    protected override ValidationResult? IsValid(
-        object? value, 
-        ValidationContext validationContext
-    ) {
-        var instance = validationContext.ObjectInstance;
-        var type = instance.GetType();
-        var propertyValue = type?.GetProperty(_propertyName)?.GetValue(instance, null);
-
-        if (propertyValue?.ToString() == _expectedValue.ToString() && value == null) {
-            var memberNames = new List<string>();
-            if (validationContext.MemberName != null) {
-                memberNames.Add(validationContext.MemberName);
-            } else {
-                Console.WriteLine("ValidationContext.MemberName is null");
-            }
-            return new ValidationResult(ErrorMessage, memberNames);
-        }
-        return ValidationResult.Success;
-    }
-}
