82cb43df6079312a2a304f0624c691bc1dde6826
Josh Elias <josh@daemontech.tools>
2024-03-22 10:01:14 -0400

    feat(data): add StringToCommunicationLink ValueResolver

--- Data/MappingProfile.cs
+++ Data/MappingProfile.cs
@@ -12,7 +12,35 @@ public class SmartOrderMappingProfile : Profile {
         CreateMap<LibraryProduct, ProductFormView>().ReverseMap();
         CreateMap<ShipLocation, ShipLocationFormView>().ReverseMap();
         CreateMap<Address, AddressFormView>().ReverseMap();
-        CreateMap<Contact, ContactFormView>().ReverseMap();
-        CreateMap<CommunicationLink, CommunicationLinkFormView>().ReverseMap();
+        CreateMap<Contact, ContactFormView>()
+            .ForMember(
+                dest => dest.Email,
+                opt => opt.MapFrom(src => src.DefaultEmail.DisplayAs)
+            )
+            .ForMember(
+                dest => dest.Phone,
+                opt => opt.MapFrom(src => src.DefaultPhone.DisplayAs)
+            )
+            .ReverseMap();
+    }
+}
+
+public class StringToCommunicationLink : IMemberValueResolver
+    <
+        ContactFormView,
+        Contact, 
+        string,
+        CommunicationLink
+    > {
+    public CommunicationLink Resolve(
+        ContactFormView source, 
+        Contact destination, 
+        string sourceMember,
+        CommunicationLink destMember, 
+        ResolutionContext context
+    ) {
+        destMember ??= new CommunicationLink();
+        destMember.DisplayAs = sourceMember;
+        return destMember;
     }
 }
