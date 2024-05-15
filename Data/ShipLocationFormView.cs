7977fdc482548b9f8247b070d9c2f6728cd707ec
Josh Elias <josh@daemontech.tools>
2024-05-15 15:04:07 -0400

    feat: replace global with ShipLocation contacts

--- Data/ShipLocationFormView.cs
+++ Data/ShipLocationFormView.cs
@@ -12,8 +12,6 @@ public class ShipLocationFormView : SmartBaseClass {
     [Required]
     public string LocationName { get; set; } = "";
 
-    [Required]
-    [ValidateComplexType]
     public ContactFormView Contact { get; set; }
     
     [Required]
